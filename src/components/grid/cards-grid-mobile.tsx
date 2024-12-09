fd24c905f241e03654038be8428706ad8a94122e
Bruno Afonso <bruno.afonso94@hotmail.com>
2024-12-09 17:28:43 +0000

    Skeleton in mobile

--- src/components/grid/cards-grid-mobile.tsx
+++ src/components/grid/cards-grid-mobile.tsx
@@ -2,23 +2,37 @@ import { TaskCard } from "@/models/task-card";
 import { useContext } from "react";
 import GeneralTaskCard from "./general-task-card";
 import { GlobalContext } from "@/context/global-context";
+import React from "react";
+import { CardTaskSkeleton } from "../skeleton/card-task-skeleton";
 
 interface Props {
   currentStatus: string;
 }
 
 const CardsGridsMobile = (props: Props) => {
-  const { cards } = useContext(GlobalContext);
+  const { cards, fetching } = useContext(GlobalContext);
 
   return (
     <div className="h-full w-full flex flex-col gap-4 overflow-y-scroll rounded-md">
-      {cards
-        .filter((card: TaskCard) => {
-          return card.status === props.currentStatus;
-        })
-        .map((card: TaskCard, index: number) => (
-          <GeneralTaskCard card={card} index={index} />
-        ))}
+      {fetching ? (
+        <>
+          {Array.from({ length: 6 }).map((_, index) => (
+            <React.Fragment key={`${props.currentStatus}-${index}`}>
+              <CardTaskSkeleton />
+            </React.Fragment>
+          ))}
+        </>
+      ) : (
+        <>
+          {cards
+            .filter((card: TaskCard) => {
+              return card.status === props.currentStatus;
+            })
+            .map((card: TaskCard, index: number) => (
+              <GeneralTaskCard card={card} index={index} />
+            ))}
+        </>
+      )}
     </div>
   );
 };
